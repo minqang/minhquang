4f7659ed909511fb4eed1d30994d9f11c393f36e
minqang <151651873+minqang@users.noreply.github.com>
2023-11-24 21:52:43 +0700

    Add files via upload

--- helo.py
+++ helo.py
@@ -1,2 +1,15 @@
-file = open("helo.txt", "r")
-print(file.read())
+import re
+
+def tim(file_path):
+    with open(file_path, 'r') as file:
+        van_ban = file.read()
+    
+    day_so = '\d+'
+    cac_so = re.findall(day_so, van_ban)
+    
+    return cac_so
+
+ket_qua = tim('helo.txt')
+
+print(ket_qua)
+
